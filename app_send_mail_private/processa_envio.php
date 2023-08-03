c710614bab0295c6cecc07f711b8a87f550755b2
Hailtom Garcia Dias <122056292+ttrikingG@users.noreply.github.com>
2023-08-02 23:03:33 -0300

    Update processa_envio.php

--- app_send_mail_private/processa_envio.php
+++ app_send_mail_private/processa_envio.php
@@ -53,15 +53,15 @@
         //Server settings
         $mail->SMTPDebug = false;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+        $mail->Host       = '';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'hailtomgdias@gmail.com';                     //SMTP username
-        $mail->Password   = 'vuqojvmcrwxikusw';                               //SMTP password
+        $mail->Username   = '';                     //SMTP username
+        $mail->Password   = ';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
         $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
         //Recipients
-        $mail->setFrom('hailtomgdias@gmail.com', 'tom remetente');
+        $mail->setFrom('', 'remetente');
         $mail->addAddress($mensagem->__get('para'));     //Add a recipient
         //$mail->addAddress('ellen@example.com');               //Name is optional
         //$mail->addReplyTo('info@example.com', 'Information');
